d1febd37b76f80e262ea5145e806edea56caad5e
Renata <renata.diniz@aluno.uece.br>
2025-10-15 18:40:51 -0300

    fix: atualizar baseUrl para GitHub Pages

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://renatabd.github.io/renata-quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
